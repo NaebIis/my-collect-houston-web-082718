03fcf4ccfd21760defdbe29f9f7496007a843d0b
John Grubbs <johnmlowery@gmail.com>
2018-08-21 18:58:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(array)
   i = 0 
-  nuarray = []
   while i < array.length  
   yield array[i]
   i = i + 1
-  nuarray << "#{array[1]}"
   end
-  return nuarray
+  return array
 end
 
