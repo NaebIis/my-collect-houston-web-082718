f3ad6903036837b7e690961251caf64f698577f0
John Grubbs <johnmlowery@gmail.com>
2018-08-21 19:00:37 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,12 +7,3 @@ def my_collect(array)
   return array
 end
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
